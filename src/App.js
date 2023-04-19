1cc9f6231294b376be8ccc0b29a179ad4de8dc59
kaganecee <kaganecee@gmail.com>
2023-04-20 00:56:29 +0300

    chore(root): removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes'
 
-import logo from './logo.svg';
-
-import './App.css';
-
 function App() {
     return <RouterProvider router={router} />;
 }
