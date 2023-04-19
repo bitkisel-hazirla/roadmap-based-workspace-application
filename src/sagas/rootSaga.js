37e29a5a232c511dd51602260a6a0b87416a0905
kaganecee <kaganecee@gmail.com>
2023-04-20 01:17:36 +0300

    chore(root): prevent start errors

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -4,9 +4,9 @@ import { setUser } from '../reducers/userReducer';
 function* fetchUser() {
     try {
         // yield => basically it's es6 async await, wait for call and continue
-        const response = yield call(API.fetchUser);
-        const user = response.content.content.credentials.name;
-        yield put(setUser(user));
+        // const response = yield call(API.fetchUser);
+        // const user = response.content.content.credentials.name;
+        // yield put(setUser(user));
     } catch (e) {
         console.log(e);
     }
