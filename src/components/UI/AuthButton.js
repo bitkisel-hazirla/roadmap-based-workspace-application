cb35af686f9ba986202f052134a1f1e4069a24f0
Emirhan Pakyardım <72696465+pakyardim@users.noreply.github.com>
2023-05-01 14:48:46 +0300

    style(auth-pages): general style and ui improvements (#9)
    * style(auth-pages): general style and ui improvements
    
    * chore(auth-pages): changed function definition styles
    
    Firstly, this commit changes the function definition to use the const = () => style, which is a more modern and concise way of defining functions. Additionally, it removes the unnecessary "dotenv" package and updates the importing statements to use local imports after global imports.
    
    Moreover, the commit replaces the "a" element with the Link component.

--- src/components/UI/AuthButton.js
+++ src/components/UI/AuthButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function AuthButton({ theme = 'primary', children, onClick, className }) {
+const AuthButton = ({ theme = 'primary', children, onClick, className }) => {
   const [bgColor, textColor] = theme === 'primary' ? ['#fff', '#597d9d'] : ['#597d9d', '#fff'];
 
   return (
     <button
-      className={`px-16 py-3 border border-gray-300 rounded-2xl ${className} `}
+      className={`px-16 py-3 border border-gray-300 ${className} `}
       style={{ backgroundColor: bgColor, color: textColor }}
       onClick={onClick}
     >
