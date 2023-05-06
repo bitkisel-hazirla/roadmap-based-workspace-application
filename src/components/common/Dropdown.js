f2b666e79cf9299ec338d014a40ba4c64ecebad5
kaganecee <kaganecee@gmail.com>
2023-05-06 21:16:08 +0300

    chore(common): eslint issues fixed & proptypes added

--- src/components/common/Dropdown.js
+++ src/components/common/Dropdown.js
@@ -1,16 +1,18 @@
-import {useState} from 'react'
-import {MdKeyboardArrowDown} from 'react-icons/md'
-export const Dropdown = ({buttonText,setActiveFilter}) => {
-    const [openDropdown, setOpenDropdown] = useState(false);
-    const handleDropdown = () => {
-        setOpenDropdown((prev)=> !prev)
-    }
-    const changeFilter = (event) => {
-        setActiveFilter(event.target.textContent);
-        handleDropdown()
-    }
-    const filters = ['Yearly','Monthly','Weekly','Daily']
-    return (
+import React, { useState } from 'react';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { string, func } from 'prop-types';
+
+export const Dropdown = ({ buttonText, setActiveFilter }) => {
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const handleDropdown = () => {
+    setOpenDropdown((prev) => !prev);
+  };
+  const changeFilter = (event) => {
+    setActiveFilter(event.target.textContent);
+    handleDropdown();
+  };
+  const filters = ['Yearly', 'Monthly', 'Weekly', 'Daily'];
+  return (
         <div className="relative inline-block text-left">
             <div>
                 <button onClick={handleDropdown} type="button" className="inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
@@ -19,11 +21,16 @@ export const Dropdown = ({buttonText,setActiveFilter}) => {
             </div>
             {openDropdown && <div className="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 p-2" >
                 <div className="flex flex-col items-start pl-4">
-                    {filters.map((item,index) => {
-                        return <button className='border-b-4' key={index} onClick={changeFilter}>{item}</button>
+                    {filters.map((item, index) => {
+                      return <button className='border-b-4' key={index} onClick={changeFilter}>{item}</button>;
                     })}
                 </div>
             </div>}
         </div>
-    )
-}
+  );
+};
+
+Dropdown.propTypes = {
+  buttonText: string,
+  setActiveFilter: func
+};
--- src/components/common/PrimaryButton.js
+++ src/components/common/PrimaryButton.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import { string, func, element } from 'prop-types';
+
 export const PrimaryButton = (props) => {
-    const { onClick, icon, text } = props;
-    return ( 
-        <button  onClick={onClick} style={{backgroundColor:'E6F2FF',color:'black'}} data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" className="flex justify-center items-center px-6 gap-2 py-2 rounded-lg ">
+  const { onClick, icon, text } = props;
+  return (
+        <button onClick={onClick} style={{ backgroundColor: 'E6F2FF', color: 'black' }} data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" className="flex justify-center items-center px-6 gap-2 py-2 rounded-lg ">
             {icon}
             {text}
             </button>
-    )
-}
+  );
+};
+
+PrimaryButton.propTypes = {
+  text: string,
+  onClick: func,
+  icon: element
+};
