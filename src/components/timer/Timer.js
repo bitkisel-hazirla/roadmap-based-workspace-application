a9abcd6123e6db6180746891a51a46702bf192af
Emirhan Pakyardım <72696465+pakyardim@users.noreply.github.com>
2023-05-06 20:59:05 +0300

    feat(timer): functionality for timer added (#14)

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,7 +1,10 @@
-export const Timer = () => {
-    return (
-        <div style={{border:'4px solid #7E828E'}} className='flex justify-center items-center rounded-full h-72 w-72'>
-            13:50
-        </div>
-    )
-}
+export const Timer = ({ time }) => {
+  return (
+    <div
+      style={{ border: '4px solid #7E828E' }}
+      className="flex justify-center items-center rounded-full h-72 w-72"
+    >
+      {time ? time : "00:00"}
+    </div>
+  );
+};
